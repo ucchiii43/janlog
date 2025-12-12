b38ee3a55cb06aae71a6e1c7d5a0dc856805933f
Ryota Uchiyama <ryota.uchiyama@my-best.com>
2025-12-13 02:01:08 +0900

    セッションのResolverを作成

--- backend/app/graphql/types/mahjong_session_type.rb
+++ backend/app/graphql/types/mahjong_session_type.rb
@@ -1,7 +1,7 @@
 module Types
   class MahjongSessionType < BaseObject
     field :id, ID, null: false
-    field :members, [Types::MemberType], null: false
     field :title, String, null: false
+    field :members, [Types::MemberType], null: false
   end
 end
